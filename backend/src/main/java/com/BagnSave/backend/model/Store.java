c553f1c52ea4a7c1486c1bb2e2c5b08d7ef04e7f
Hunter Fromont <fromonhunt1@ecs.vuw.ac.nz>
2026-07-20 11:48:33 +1200

    Completed Vendor and Store Models

--- backend/src/main/java/com/BagnSave/backend/model/Store.java
+++ backend/src/main/java/com/BagnSave/backend/model/Store.java
@@ -14,5 +14,48 @@ public class Store {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    private String name;
+    private boolean isEnabled;
+    private int vendorId;    
+
+    public Store(){}
     
+    public Store(int id, String name, boolean isEnabled, int vendorId){
+        this.id = id;
+        this.name = name;
+        this.isEnabled = isEnabled;
+        this.vendorId = vendorId;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id; 
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public boolean getIsEnabled(){
+        return isEnabled;
+    }
+
+    public void setIsEnabled(boolean isEnabled){
+        this.isEnabled = isEnabled;
+    }
+    
+    public int getVendorId(){
+        return vendorId;
+    }
+
+    public void setVendorId(int vendorId){
+        this.vendorId = vendorId;
+    }
 }

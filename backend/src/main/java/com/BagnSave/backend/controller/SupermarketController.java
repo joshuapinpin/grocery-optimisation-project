13a8a72e964a76842257275d5e1ef764dfa35255
Hunter Fromont <fromonhunt1@ecs.vuw.ac.nz>
2026-07-20 12:38:32 +1200

    Implemented SupermarketServiceController Logic

--- backend/src/main/java/com/BagnSave/backend/controller/SupermarketController.java
+++ backend/src/main/java/com/BagnSave/backend/controller/SupermarketController.java
@@ -1,5 +1,27 @@
 package com.BagnSave.backend.controller;
 
+import com.BagnSave.backend.dto.StoreDTO;
+import com.BagnSave.backend.service.SupermarketService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/supermarkets")
+@CrossOrigin(origins = "*") // To-Do: tighten to vercel URL
 public class SupermarketController {
     
+    private final SupermarketService supermarketService;
+
+    public SupermarketController(SupermarketService supermarketService) {
+        this.supermarketService = supermarketService;
+    }
+
+    @GetMapping
+    public List<StoreDTO> getAllStores() {
+        return supermarketService.getAllStores();
+    }
 }

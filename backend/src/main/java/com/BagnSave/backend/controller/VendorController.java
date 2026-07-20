bd89716696dd9412538b6372deac209d9af2010d
Hunter Fromont <fromonhunt1@ecs.vuw.ac.nz>
2026-07-20 23:34:33 +1200

    Implemented VendorDTO, VendorService and VendorController

--- backend/src/main/java/com/BagnSave/backend/controller/VendorController.java
+++ backend/src/main/java/com/BagnSave/backend/controller/VendorController.java
@@ -1,5 +1,27 @@
 package com.BagnSave.backend.controller;
 
+import com.BagnSave.backend.dto.VendorDTO;
+import com.BagnSave.backend.service.VendorService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/vendors")
+@CrossOrigin(origins = "*") // To-Do: tighten to vercel URL
 public class VendorController {
-    
+
+    private final VendorService vendorService;
+
+    public VendorController(VendorService vendorService) {
+        this.vendorService = vendorService;
+    }
+
+    @GetMapping
+    public List<VendorDTO> getAllVendors() {
+        return vendorService.getAllVendors();
+    }
 }
--- backend/src/main/java/com/BagnSave/backend/service/VendorService.java
+++ backend/src/main/java/com/BagnSave/backend/service/VendorService.java
@@ -1,5 +1,33 @@
 package com.BagnSave.backend.service;
 
+import com.BagnSave.backend.dto.VendorDTO;
+import com.BagnSave.backend.model.Vendor;
+import com.BagnSave.backend.repository.VendorRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class VendorService {
     
+    private final VendorRepository vendorRepository;
+
+    public VendorService(VendorRepository vendorRepository){
+        this.vendorRepository = vendorRepository;
+    }
+
+    public List<VendorDTO> getAllVendors() {
+        List<Vendor> vendors = vendorRepository.findAll();
+
+        return vendors.stream()
+                .map(this::toDTO)
+                .toList();
+    }
+
+    private VendorDTO toDTO(Vendor vendor) {
+        return new VendorDTO(
+                vendor.getId(),
+                vendor.getName()
+        );
+    }
 }

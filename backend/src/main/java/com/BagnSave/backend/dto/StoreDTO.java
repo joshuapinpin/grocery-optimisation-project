1102a426fd6434ecdd01ace81631bf11bb1583e1
Hunter Fromont <fromonhunt1@ecs.vuw.ac.nz>
2026-07-20 12:03:27 +1200

    Completed StoreDTO, VendorDTO not required

--- backend/src/main/java/com/BagnSave/backend/dto/StoreDTO.java
+++ backend/src/main/java/com/BagnSave/backend/dto/StoreDTO.java
@@ -2,4 +2,49 @@ package com.BagnSave.backend.dto;
 
 public class StoreDTO {
     
+    private int id;
+    private String name;
+    private boolean isEnabled;
+    private String vendorName;
+
+    public StoreDTO() {}
+
+    public StoreDTO(int id, String name, boolean isEnabled, String vendorName) {
+        this.id = id;
+        this.name = name;
+        this.isEnabled = isEnabled;
+        this.vendorName = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public boolean getIsEnabled() {
+        return isEnabled;
+    }
+
+    public void setIsEnabled(boolean isEnabled) {
+        this.isEnabled = isEnabled;
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
 }

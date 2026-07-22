e6c830e4c98f67790876c7ae036135bbd730390a
Hunter Fromont <fromonhunt1@ecs.vuw.ac.nz>
2026-07-23 01:35:05 +1200

    Implemented Product Controller and Service

--- backend/src/main/java/com/BagnSave/backend/product/ProductService.java
+++ backend/src/main/java/com/BagnSave/backend/product/ProductService.java
@@ -1,5 +1,36 @@
 package com.BagnSave.backend.product;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.BagnSave.backend.product.dto.ProductDTO;
+
+@Service
 public class ProductService {
-    
+
+     private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<ProductDTO> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream()
+                .map(this::toDTO)
+                .toList();
+    }
+
+    public ProductDTO toDTO(Product product) {
+        return new ProductDTO(
+            product.getId(),
+            product.getName(),
+            product.getBrand(),
+            product.getUnit(),
+            product.getSize(),
+            product.getRedirectedTo()
+        );
+    }
 }

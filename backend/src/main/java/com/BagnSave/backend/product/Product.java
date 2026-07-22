f17fb1971127afa64ed104ed0b778ed121edca35
Hunter Fromont <fromonhunt1@ecs.vuw.ac.nz>
2026-07-23 01:25:17 +1200

    Implemented Product Type and DTO

--- backend/src/main/java/com/BagnSave/backend/product/Product.java
+++ backend/src/main/java/com/BagnSave/backend/product/Product.java
@@ -1,5 +1,77 @@
 package com.BagnSave.backend.product;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "public_products")
 public class Product {
-    
+
+    @Id
+    private int id;
+    private String name;
+    private String brand;
+    private String unit;
+    private String size;
+    private int redirectedTo;
+
+    public Product() {} // private ?
+
+    public Product(int id, String name, String brand, String unit, String size, int redirectedTo) {
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+        this.unit = unit;
+        this.size = size;
+        this.redirectedTo = redirectedTo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public int getRedirectedTo() {
+        return redirectedTo;
+    }
+
+    public void setRedirectedTo(int redirectedTo) {
+        this.redirectedTo = redirectedTo;
+    }
 }
--- backend/src/main/java/com/BagnSave/backend/product/dto/ProductDTO.java
+++ backend/src/main/java/com/BagnSave/backend/product/dto/ProductDTO.java
@@ -2,4 +2,70 @@ package com.BagnSave.backend.product.dto;
 
 public class ProductDTO {
     
+    private int id;
+    private String name;
+    private String brand;
+    private String unit;
+    private String size;
+    private int redirectedTo;
+
+    public ProductDTO() {}
+
+    public ProductDTO(int id, String name, String brand, String unit, String size, int redirectedTo) {
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+        this.unit = unit;
+        this.size = size;
+        this.redirectedTo = redirectedTo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public int getRedirectedTo() {
+        return redirectedTo;
+    }
+
+    public void setRedirectedTo(int redirectedTo) {
+        this.redirectedTo = redirectedTo;
+    }
+
 }

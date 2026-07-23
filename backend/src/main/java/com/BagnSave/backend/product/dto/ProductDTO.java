34a05387388c2ddf703177189b42f9c1cfbd3286
Hunter Fromont <fromonhunt1@ecs.vuw.ac.nz>
2026-07-23 17:29:15 +1200

    lombok Implemented to Product and ProductDTO

--- backend/src/main/java/com/BagnSave/backend/product/dto/ProductDTO.java
+++ backend/src/main/java/com/BagnSave/backend/product/dto/ProductDTO.java
@@ -1,71 +1,13 @@
 package com.BagnSave.backend.product.dto;
 
+import lombok.Value;
+
+@Value
 public class ProductDTO {
-    
-    private Integer id;
-    private String name;
-    private String brand;
-    private String unit;
-    private String size;
-    private Integer redirectedTo;
-
-    public ProductDTO() {}
-
-    public ProductDTO(Integer id, String name, String brand, String unit, String size, Integer redirectedTo) {
-        this.id = id;
-        this.name = name;
-        this.brand = brand;
-        this.unit = unit;
-        this.size = size;
-        this.redirectedTo = redirectedTo;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public String getUnit() {
-        return unit;
-    }
-
-    public void setUnit(String unit) {
-        this.unit = unit;
-    }
-
-    public String getSize() {
-        return size;
-    }
-
-    public void setSize(String size) {
-        this.size = size;
-    }
-
-    public Integer getRedirectedTo() {
-        return redirectedTo;
-    }
-
-    public void setRedirectedTo(Integer redirectedTo) {
-        this.redirectedTo = redirectedTo;
-    }
-
+    Integer id;
+    String name;
+    String brand;
+    String unit;
+    String size;
+    Integer redirectedTo;
 }

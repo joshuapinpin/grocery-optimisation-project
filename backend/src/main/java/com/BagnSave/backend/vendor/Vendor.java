b6cce7b06d102f937a94bf59bcf58027f4b7f473
Hunter Fromont <fromonhunt1@ecs.vuw.ac.nz>
2026-07-23 17:46:00 +1200

    lombok Implemented to Vendor and VendorDTO

--- backend/src/main/java/com/BagnSave/backend/vendor/Vendor.java
+++ backend/src/main/java/com/BagnSave/backend/vendor/Vendor.java
@@ -1,41 +1,29 @@
 package com.BagnSave.backend.vendor;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import lombok.Getter;
 
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(onlyExplicitlyIncluded = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Entity
 @Table(name = "public_vendors")
 public class Vendor {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    @EqualsAndHashCode.Include
+    @ToString.Include
+    private Integer id;
 
+    @ToString.Include
     private String name;
-
-    public Vendor(){}
-
-    public Vendor(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public int getId(){
-        return id;
-    }
-
-    public void setId(int id){
-        this.id = id;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    public void setName(String name){
-        this.name = name;
-    }
 }

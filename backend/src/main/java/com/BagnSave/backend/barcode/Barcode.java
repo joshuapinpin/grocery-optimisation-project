6175094023232a5a58fc90cd707f407e2fc86c05
Hunter Fromont <fromonhunt1@ecs.vuw.ac.nz>
2026-07-26 21:33:53 +1200

    Implemented Bardcode.java

--- backend/src/main/java/com/BagnSave/backend/barcode/Barcode.java
+++ backend/src/main/java/com/BagnSave/backend/barcode/Barcode.java
@@ -1,5 +1,28 @@
 package com.BagnSave.backend.barcode;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import lombok.Getter;
+
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(onlyExplicitlyIncluded = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity
+@Table(name = "public_barcodes")
 public class Barcode {
     
+   @Id
+   @EqualsAndHashCode.Include
+   @ToString.Include
+   private String barcode; 
+
+   private Integer productId;
 }

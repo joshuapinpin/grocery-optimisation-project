a2a508df487ce9a00e455d25e83366a1e386dbb7
Hunter Fromont <fromonhunt1@ecs.vuw.ac.nz>
2026-07-26 23:07:36 +1200

    Implemented CollectionMember and CollectionMemberDTO

--- backend/src/main/java/com/BagnSave/backend/collectionmember/CollectionMemberId.java
+++ backend/src/main/java/com/BagnSave/backend/collectionmember/CollectionMemberId.java
@@ -1,5 +1,19 @@
 package com.BagnSave.backend.collectionmember;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-public class CollectionMemberId {
+import java.io.Serializable;
+
+@Getter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class CollectionMemberId implements Serializable {
     
+    private Integer collectionId;
+    private Integer productId;
 }
--- backend/src/main/java/com/BagnSave/backend/collectionmember/dto/CollectionMemberDTO.java
+++ backend/src/main/java/com/BagnSave/backend/collectionmember/dto/CollectionMemberDTO.java
@@ -1,5 +1,9 @@
 package com.BagnSave.backend.collectionmember.dto;
 
+import lombok.Value;
+
+@Value
 public class CollectionMemberDTO {
-    
+    Integer collectionId;
+    Integer productId;
 }

81835522f123b9380aa8b47b6b32f6bbf6e6a99c
Hunter Fromont <fromonhunt1@ecs.vuw.ac.nz>
2026-07-26 17:22:40 +1200

    Created Composite Key PriceId

--- backend/src/main/java/com/BagnSave/backend/price/PriceId.java
+++ backend/src/main/java/com/BagnSave/backend/price/PriceId.java
@@ -1,5 +1,20 @@
 package com.BagnSave.backend.price;
 
-public class PriceId {
-    
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Getter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class PriceId implements Serializable {
+
+    private Integer storeId;
+    private Integer productId;
 }

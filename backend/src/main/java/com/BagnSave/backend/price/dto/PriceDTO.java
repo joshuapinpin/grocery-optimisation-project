bd6e2b6bbcc7b0e0c4ebf101eaf587a3d2f5bdb9
Hunter Fromont <fromonhunt1@ecs.vuw.ac.nz>
2026-07-26 17:37:08 +1200

    PriceDTO Complete

--- backend/src/main/java/com/BagnSave/backend/price/dto/PriceDTO.java
+++ backend/src/main/java/com/BagnSave/backend/price/dto/PriceDTO.java
@@ -1,5 +1,19 @@
 package com.BagnSave.backend.price.dto;
 
+import java.time.OffsetDateTime;
+import lombok.Value;
+
+@Value
 public class PriceDTO {
-    
+    Integer storeId;
+    Integer productId;
+    Integer originalPriceCent;
+    Integer salePriceCent;
+    Integer clubPriceCent;
+    Integer onlinePriceCent;
+    Integer multibuyPriceInteger;
+    Integer multibuyQuantity;
+    Integer clubMultibuyPriceCent;
+    Integer clubMultibuyQuantity;
+    OffsetDateTime updatedAt;
 }

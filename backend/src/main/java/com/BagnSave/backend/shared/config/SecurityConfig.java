9fe72a8ad9a70f567277650a4ebab61f93c5a08c
pinpinjosh <pinpinjosh@ecs.vuw.ac.nz>
2026-07-28 10:06:56 +1200

    feat(oauth): added configurations for oauth2 client

--- backend/src/main/java/com/BagnSave/backend/shared/config/SecurityConfig.java
+++ backend/src/main/java/com/BagnSave/backend/shared/config/SecurityConfig.java
@@ -14,7 +14,8 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()
+                    .requestMatchers("/api/**").permitAll()
+                    .anyRequest().permitAll()
             );
         return http.build();
     }

21017e7dd8e4b60c0ad8b0d5bcdbf58c46699b42
Hunter Fromont <fromonhunt1@ecs.vuw.ac.nz>
2026-08-04 18:35:03 +1200

    Commented out non-existance pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Homepage from './pages/Homepage'
 import MockProducts from './pages/MockProducts'
 import Login from './pages/Login'
-import SelectItems from './pages/SelectItems'
+// TODO: uncomment once these pages exist
+// import SelectItems from './pages/SelectItems'
 import SelectStores from './pages/SelectStores'
-import Compare from './pages/Compare'
-import Register from './pages/Register'
-import NotFound from './pages/NotFound'
+// import Compare from './pages/Compare'
+// import Register from './pages/Register'
+// import NotFound from './pages/NotFound'
 
 function App() {
   return (
@@ -17,12 +18,13 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/mock-products" element={<MockProducts />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/select-items" element={<SelectItems />} />
         <Route path="/select-stores" element={<SelectStores />} />
-        <Route path="/compare" element={<Compare />} />
-        <Route path="/register" element={<Register />} />
+        {/* TODO: uncomment once these pages exist */}
+        {/* <Route path="/select-items" element={<SelectItems />} /> */}
+        {/* <Route path="/compare" element={<Compare />} /> */}
+        {/* <Route path="/register" element={<Register />} /> */}
         {/* keep this last, it matches anything the routes above didn't */}
-        <Route path="*" element={<NotFound />} />
+        {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
     </BrowserRouter>
   )

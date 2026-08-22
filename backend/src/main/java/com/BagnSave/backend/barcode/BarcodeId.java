df7024b665fbc8aff324d48e6482368b21f1fc11
Hunter Fromont <fromonhunt1@ecs.vuw.ac.nz>
2026-08-22 22:46:35 +1200

    Implemented BarcodeId.java

--- backend/src/main/java/com/BagnSave/backend/barcode/BarcodeId.java
+++ backend/src/main/java/com/BagnSave/backend/barcode/BarcodeId.java
@@ -1,5 +1,19 @@
 package com.BagnSave.backend.barcode;
 
-public class BarcodeId {
-    
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.io.Serializable;
+
+@Getter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class BarcodeId implements Serializable {
+
+    private Integer productId;
+    private Integer barcodeId;
 }
